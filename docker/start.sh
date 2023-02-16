7e924d481ab8d110021f805db6da3c09218a4624
Steven McDonald <steven.mcdonald@gmail.com>
2023-02-16 11:03:11 -0800

    don't need to check for depot_tools, it's included

--- docker/start.sh
+++ docker/start.sh
@@ -3,10 +3,10 @@
 set -e
 
 [[ ! -d "$CHROMIUM_SRC_ROOT" ]] && echo "chromium directory not mounted!" && exit 1
-[[ ! -d "$DEPOT_TOOLS_ROOT" ]] && echo "depot_tools directory not mounted!" && exit 2
+#[[ ! -d "$DEPOT_TOOLS_ROOT" ]] && echo "depot_tools directory not mounted!" && exit 2
 [[ ! -d /build/envoy ]] && echo "envoy directory not mounted!" && exit 3
 
-/usr/sbin/icecc-scheduler -d -vvv -l /tmp/icecc-scheduler.log
+#/usr/sbin/icecc-scheduler -d -vvv -l /tmp/icecc-scheduler.log
 
 cd /build/envoy
 exec /bin/bash
